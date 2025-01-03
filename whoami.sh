bfeaa55ee3d6304305c827c009871eca05c2a8a0
pedro arribas <104268930+pedroarrman@users.noreply.github.com>
2025-01-03 19:30:18 +0100

    Add files via upload

---
+++ whoami.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+usuario=`whoami`
+if [ $usuario == "Manolo" ]
+then
+	echo "Hola $usuario me debes 500 euros"
+else
+	echo "hasta luego, dile a manolo que lo estoy esperando"
+fi
+
+ctrl+x+k
+
+Manolo.sh
+
+chmod 777 Manolo.sh
+
+./Manolo.sh
